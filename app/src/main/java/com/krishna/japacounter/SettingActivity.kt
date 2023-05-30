ccfe494f6b17bd305c406d43e19c9ec642927018
kuldeep-IT <rathod99244@gmail.com>
2023-05-31 01:00:38 +0530

    - package update

--- app/src/main/java/com/krishna/japacounter/SettingActivity.kt
+++ app/src/main/java/com/krishna/japacounter/SettingActivity.kt
@@ -1,13 +1,17 @@
 package com.krishna.japacounter
 
+import android.content.DialogInterface
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.krishna.japacounter.MainActivity.Companion.IS_RESET
 import com.krishna.japacounter.databinding.ActivitySettingBinding
 
@@ -17,7 +21,7 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var actionBar: ActionBar
 
     lateinit var sharedP: SharedPreferences
-    lateinit var myEdit : SharedPreferences.Editor
+    lateinit var myEdit: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.SettingTheme)
@@ -26,7 +30,8 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_setting)
 
         actionBar = supportActionBar!!
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#CCCCFF")))
         actionBar.title = "Settings"
 
         sharedP = getSharedPreferences("MySharedPref", MODE_PRIVATE)
@@ -42,8 +47,8 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        when (item.itemId){
-            android.R.id.home ->{
+        when (item.itemId) {
+            android.R.id.home -> {
                 this.finish()
                 return true
             }
@@ -53,16 +58,27 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.reset ->{
-                myEdit.putString(MainActivity.TODAY_JAPA, "0")
-                myEdit.putString(MainActivity.TOTAL_JAPA, "0")
+        when (v?.id) {
+            R.id.reset -> {
+                MaterialAlertDialogBuilder(this,
+                    R.style.RoundShapeAppearance
+                    ).setTitle("Reset Japa Counter")
+                    .setMessage("All the count has been reset to 0.")
+                    .setPositiveButton("Reset", DialogInterface.OnClickListener { dialog, which ->
+                        myEdit.putString(MainActivity.TODAY_JAPA, "0")
+                        myEdit.putString(MainActivity.TOTAL_JAPA, "0")
 
-                myEdit.commit();
+                        myEdit.commit()
 
-                IS_RESET = true
+                        IS_RESET = true
 
-                showToast("Data is successfully Reset")
+//                        showToast("Data is successfully Reset")
+                        dialog.dismiss()
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                    }).show()
+
             }
         }
     }
